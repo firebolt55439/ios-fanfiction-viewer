94ea2ace12e602d028be476f6fa66e8f1c197989
firebolt55439 <sumer112@gmail.com>
2015-12-17 19:06:17 -0800

    Initial Commit

---
+++ Fanfiction/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Fanfiction
+//
+//  Created by Sumer Kohli on 12/17/15.
+//  Copyright © 2015 Sumer Kohli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Fanfiction/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Fanfiction
+//
+//  Created by Sumer Kohli on 12/17/15.
+//  Copyright © 2015 Sumer Kohli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
